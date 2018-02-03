e505fae6b06710e0589002f332f7a514048c85f7
SuperSodaSea <bobby825@126.com>
2018-02-03 11:08:19 +0800

    Update Example

--- example/XML_DOMReader/XML_DOMReader.cpp
+++ example/XML_DOMReader/XML_DOMReader.cpp
@@ -52,6 +52,7 @@ int main(int argc, char** argv) {
     try {
         
         if(argc < 2) throw InvalidArgumentException("File name needed");
+        
         for(int i = 1; i < argc; ++i) {
             
             auto data = readFile(argv[i]);
@@ -61,12 +62,7 @@ int main(int argc, char** argv) {
             
         }
         
-    } catch(std::exception& e) {
-        
-        std::cerr << e.what() << std::endl;
-        return 1;
-        
-    }
+    } catch(std::exception& e) { std::cerr << e.what() << std::endl; return 1; }
     
     return 0;
     
--- example/XML_SAXReader/XML_SAXReader.cpp
+++ example/XML_SAXReader/XML_SAXReader.cpp
@@ -111,6 +111,7 @@ int main(int argc, char** argv) {
     try {
         
         if(argc < 2) throw InvalidArgumentException("File name needed");
+        
         for(int i = 1; i < argc; ++i) {
             
             auto data = readFile(argv[i]);
@@ -120,12 +121,7 @@ int main(int argc, char** argv) {
             
         }
         
-    } catch(std::exception& e) {
-        
-        std::cerr << e.what() << std::endl;
-        return 1;
-        
-    }
+    } catch(std::exception& e) { std::cerr << e.what() << std::endl; return 1; }
     
     return 0;
     
